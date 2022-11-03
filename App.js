c61f3c44be495f1ceef920a25bafaff61ba51995
Wojtek <wojwoj1010@wp.pl>
2022-11-03 17:54:50 +0100

    aaa

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js develop brach!</Text>
+      <Text>Open up App.js develop brach! aaa</Text>
       <StatusBar style="auto" />
     </View>
   );
